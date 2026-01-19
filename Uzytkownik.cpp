4b42e750f842c093e0dd6a95690497483df01612
Przemo320 <95084200+Przemo320@users.noreply.github.com>
2026-01-19 20:11:59 +0000

    Poprawienie funkcjonowania aplikacji

--- Uzytkownik.cpp
+++ Uzytkownik.cpp
@@ -2,8 +2,22 @@
 #include <string>
 #include <fstream>
 #include <list>
+#include <sstream>
 
 using namespace std;
+
+const string KLUCZ_SZYFROWANIA = "klucz";
+
+string xorString(const string& input, const string& key)
+{
+    string output = input;
+    size_t key_len = key.length();
+    for (size_t i = 0; i < input.length(); ++i)
+    {
+        output[i] = input[i] ^ key[i % key_len];
+    }
+    return output;
+}
 
 Uzytkownik::Uzytkownik(string i, string n, string ID, string h)
     : imie(i), nazwisko(n), id(ID), haslo(h)
@@ -12,5 +26,25 @@ Uzytkownik::Uzytkownik(string i, string n, string ID, string h)
 bool Uzytkownik::czyPoprawneDaneLogowania(const string &login,
                                           const string &haslo) const
 {
-  return (id == login && this->haslo == haslo);
+    return (id == login && this->haslo == haslo);
+}
+
+void Uzytkownik::zapiszUzytkownika(const list<Uzytkownik>& uzytkownicy)
+{
+    ofstream plik("uzytkownicy.txt");
+    if (!plik.is_open()) return;
+
+    for (const auto& u : uzytkownicy)
+    {
+        string haslo_zaszyfrowane = xorString(u.haslo, KLUCZ_SZYFROWANIA);
+        plik << u.id << ";" << u.imie << ";" << u.nazwisko << ";" << haslo_zaszyfrowane << endl;
+    }
+    plik.close();
+}
+
+void Uzytkownik::usunUzytkownika(list<Uzytkownik>& uzytkownicy, const string& id)
+{
+    uzytkownicy.remove_if([id](const Uzytkownik& u) {
+        return u.id == id;
+    });
 }
--- Uzytkownik.h
+++ Uzytkownik.h
@@ -1,37 +1,28 @@
-#ifndef Uzytkownik_H
-#define Uzytkownik_H
+#ifndef UZYTKOWNIK_H
+#define UZYTKOWNIK_H
+
 #include <string>
 #include <list>
-#include <fstream>
 
 class Uzytkownik
 {
 public:
-  Uzytkownik(std::string i, std::string n, std::string ID, std::string h);
-  bool czyPoprawneDaneLogowania(const std::string &login,
-                                const std::string &haslo) const;
+    std::string imie;
+    std::string nazwisko;
+    std::string id;
+    std::string haslo;
 
-  std::string imie;
-  std::string nazwisko;
-  std::string id;
-  std::string haslo;
+    Uzytkownik(std::string i, std::string n, std::string ID, std::string h);
 
-  static void zapiszUzytkownika(const std::list<Uzytkownik> &uzytkownicy)
-  {
-    std::ofstream plik("urzytkownicy.txt");
-    for(const auto &u : uzytkownicy)
-      {
-        plik << u.id << ";" << u.imie << ";" << u.nazwisko << ";" << u.haslo
-             << ";" << std::endl;
-      }
-    plik.close();
-  }
+    bool czyPoprawneDaneLogowania(const std::string &login,
+                                  const std::string &haslo) const;
 
-  static void
-  usunUzytkownika(std::list<Uzytkownik> &uzytkownicy, const std::string &id)
-  {
-    uzytkownicy.remove_if([&id](const Uzytkownik &u) { return u.id == id; });
-  }
+    static void zapiszUzytkownika(const std::list<Uzytkownik>& uzytkownicy);
+    static void usunUzytkownika(std::list<Uzytkownik>& uzytkownicy, const std::string& id);
 };
 
-#endif
+extern const std::string KLUCZ_SZYFROWANIA;
+
+std::string xorString(const std::string& input, const std::string& key);
+
+#endif // UZYTKOWNIK_H
